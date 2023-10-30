78ca362da45ba7af808916a0fb853fa960702bde
codewitgabi <gabrielmicheal497@gmail.com>
2023-10-30 15:49:07 +0100

    Added frontend server to CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,11 @@ app.use("/chat", verifyAuth)
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:8158"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:8158",
+      "https://hankochat.vercel.app"
+    ],
     credentials: false,
     maxHttpBufferSize: 1e8
   }
